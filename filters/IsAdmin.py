8aa3ac6b1d994a8a9f3c53f96abb31a2a6f6ba00
aksen <aksenov.andrey26@gmail.com>
2024-03-20 17:26:11 +0400

    bug

--- filters/IsAdmin.py
+++ filters/IsAdmin.py
@@ -10,7 +10,7 @@ class IsAdmin(BaseFilter):
     async def __call__(self, msg: Message):
         user = str(msg.from_user.id)
         status = await MasterDB.get_status(user)
-        if status != 'admin' and user == potisepents.admin_id:
+        if status != 'admin' and user != potisepents.admin_id:
             await msg.answer('Только для админов!')
             return False
         return True
